5444f4dd407c9258dafef42fcb557e42e883795f
dpayton23 <darien.payton71@gmail.com>
2019-03-29 12:36:06 -0400

    added PropTypes

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const Tab = props => {
   return (
@@ -12,6 +13,9 @@ const Tab = props => {
   );
 };
 
-// Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tab: PropTypes.object,
+  selectedTab: PropTypes.object
+}
 
 export default Tab;
