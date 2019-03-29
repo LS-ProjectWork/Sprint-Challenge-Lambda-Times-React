0f7392009db61a827731dc82ca8f0d8e12c679ae
dpayton23 <darien.payton71@gmail.com>
2019-03-29 12:52:09 -0400

    added proptypes

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types'
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -13,5 +15,10 @@ const Tabs = props => {
   );
 };
 
-// Make sure to use PropTypes to validate your types!
+Tab.propTypes = {
+  tabsObject: PropTypes.shape({
+    tabData: PropTypes.string
+  })
+}
+
 export default Tabs;
